fe085f074ab63f4d3af155de17bf95e45664d0f4
eddydevops <eddypsc@gmail.com>
2022-05-01 13:45:15 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # git-it-done
+
+![quality score](https://api.codiga.io/project/33244/score/svg)
+
+![code grade](https://api.codiga.io/project/33244/status/svg)
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,28 @@
-var getUserRepos = function() {
+var getUserRepos = function(user) {
     // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
 
-   fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
 
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+
+    event.preventDefault();
+
+    console.log(event);
 };
+
+userFormEl.addEventListener("submit", formSubmitHandler);
 
   
 
